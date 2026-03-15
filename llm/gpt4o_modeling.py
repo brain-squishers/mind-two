9ab38ce9526dc2080bcb4623bbcf8efb29597fe1
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2026-03-15 06:37:14 -0400

    Maded gpt work in async thread to prevent frame crash/freeze

--- llm/gpt4o_modeling.py
+++ llm/gpt4o_modeling.py
@@ -1,14 +1,15 @@
+import asyncio
+
 from .api_wrap import OpenAIAPIWrapper
 
-class GPT4o():
+
+class GPT4o:
     def __init__(self, model="gpt-4o-2024-05-13", device="cuda") -> None:
-
-        self.model =  OpenAIAPIWrapper(model)
-        
-
+        self.model = OpenAIAPIWrapper(model)
 
     async def generate(self, query):
-
-        response, _ = self.model.get_completion(query)
-        
+        response, _ = await asyncio.to_thread(
+            self.model.get_completion,
+            query,
+        )
         return response
